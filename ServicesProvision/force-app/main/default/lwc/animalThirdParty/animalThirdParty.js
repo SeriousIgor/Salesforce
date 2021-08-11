1e559f8a57f6790d77f1d381372a53d8161d66a3
SeriousIgor <Seriousigorok@gmail.com>
2021-08-11 13:07:54 +0300

    Some improvements in controller

--- ServicesProvision/force-app/main/default/lwc/animalThirdParty/animalThirdParty.js
+++ ServicesProvision/force-app/main/default/lwc/animalThirdParty/animalThirdParty.js
@@ -1,18 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import getAnimalId from '@salesforce/apex/AnimalThirdPartyController.getAnimalId';
 import insertAnimal from '@salesforce/apex/AnimalThirdPartyController.insertAnimal';
 import checkForDublicate from '@salesforce/apex/AnimalThirdPartyController.checkForDublicate';
 import replaceValueInRecord from '@salesforce/apex/AnimalThirdPartyController.replaceValueInRecord';
 
 export default class AnimalThirdParty extends LightningElement {
+    externalId = '1';
+    animalObj
     animalInfoLabel;
-    handleClick(event) {
-        this.animalInfoLabel = event.target.label;
-    }
-
+    error;
     popUpWindow = false;
     handleClick(){
+        getAnimalId({extId: this.externalId})
+            .then((result) =>{
+                this.animalObj = result;
+                console.log(this.animalObj);
+                this.error = undefined;
+            })
+            .catch((error) => {
+                this.error = error;
+                this.animalObj = undefined;
+            });        
+
+        this.animalInfoLabel = this.animalObj;
         this.popUpWindow = true;
+    }
+    callmeout(){
+        insertAnimal();
+        this.popUpWindow = false;
     }
     closeModal(){
         this.popUpWindow = false;
